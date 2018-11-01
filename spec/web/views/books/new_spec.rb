87475772f2a4196bca9cf93038d491938cf69fda
Luiz <lfernando.bosso@gmail.com>
2018-10-31 22:48:05 -0300

    Adds option to create books with specs

---
+++ spec/web/views/books/new_spec.rb
@@ -0,0 +1,17 @@
+# spec/web/views/books/new_spec.rb
+require 'ostruct'
+require_relative '../../../../apps/web/views/books/new'
+
+RSpec.describe Web::Views::Books::New do
+  let(:params)    { OpenStruct.new(valid?: false, error_messages: ['Title must be filled', 'Author must be filled']) }
+  let(:exposures) { Hash[params: params] }
+  let(:template)  { Hanami::View::Template.new('apps/web/templates/books/new.html.erb') }
+  let(:view)      { Web::Views::Books::New.new(template, exposures) }
+  let(:rendered)  { view.render }
+
+  it 'displays list of errors when params contains errors' do
+    expect(rendered).to include('There was a problem with your submission')
+    expect(rendered).to include('Title must be filled')
+    expect(rendered).to include('Author must be filled')
+  end
+end
